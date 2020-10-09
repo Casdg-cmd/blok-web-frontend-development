117fd84c904384e47e5737df2a6783dabd9bebad
Casdg-cmd <70708335+Casdg-cmd@users.noreply.github.com>
2020-10-09 15:02:28 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
 // JavaScript Document
+var sizeOnScroll = document.querySelector(".bedrijfslogo");
+
+const checkpoint = 100;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+  	var element = document.getElementByClass("bedrijfslogo");
+  element.classList.add("logoscroll");
+  } 
+  document.querySelector(".front").style.opacity = opacity;
+});
